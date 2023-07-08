0576d08c7d27b5bfaee4fad4e18fd5701905e116
Adailton Moura <contatoadailtonmoura@gmail.com>
2023-07-08 01:59:33 -0300

    adding images components in header file

--- resources/js/Pages/Layout/Header.vue
+++ resources/js/Pages/Layout/Header.vue
@@ -1,7 +1,7 @@
 <template>
-    <header class="w-full h-14 sm:bg-blue_main-500  mb-4 px-6 flex justify-between items-center">
-        <img :src="'../storage/images/ufs.svg'" alt="ufs">
-        <img :src="'../storage/images/avatar.jpg'" alt="avatar" class="rounded-full h-8 w-8">
-        <slot />
+    <header class="w-full h-14 sm:bg-blufs-500  mb-4 px-6 flex justify-between items-center">
+        <Logo :width="38" :height="31" fill_stroke="#004999"/>
+        <ProfilePicture :style="'rounded-full h-8 w-8'"/>
+        <slot/>
     </header>
 </template>
