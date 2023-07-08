60a80517357ad1ae3b0c0b750084680be44b0217
Adailton Moura <contatoadailtonmoura@gmail.com>
2023-07-08 01:57:07 -0300

    create images components

---
+++ resources/js/Pages/Components/ProfilePicture.vue
@@ -0,0 +1,16 @@
+<template>
+    <img :src="'../storage/images/avatar.jpg'" alt="avatar" :class="style">
+</template>
+
+<script>
+export default {
+    name: "ProfilePicture",
+    props: {
+        style: String
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
